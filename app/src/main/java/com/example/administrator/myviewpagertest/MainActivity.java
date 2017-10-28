ca2be9cff7cc994ed8adb883effbcbb18a4ddc3d
Brad Chao <brad@brad.tw>
2017-10-28 15:18:18 +0800

    201710281518

--- app/src/main/java/com/example/administrator/myviewpagertest/MainActivity.java
+++ app/src/main/java/com/example/administrator/myviewpagertest/MainActivity.java
@@ -4,24 +4,34 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private ArrayList<View> views;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //tv1 = (TextView)findViewById(R.id.tv1);
+
         viewPager = (ViewPager)findViewById(R.id.viewPager);
         initViewPager();
     }
+
+    public void test1(View view){
+        tv1.setText("Hello, Brad");
+    }
+
 
     private void initViewPager(){
         LayoutInflater inflater = LayoutInflater.from(this);
@@ -31,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         View page2 = inflater.inflate(R.layout.page2, null);
         View page3 = inflater.inflate(R.layout.page3, null);
         views.add(page1);views.add(page2);views.add(page3);
+
+        tv1 = (TextView)page1.findViewById(R.id.tv1);
+
 
         viewPager.setAdapter(new MyPagerAdapter());
 
