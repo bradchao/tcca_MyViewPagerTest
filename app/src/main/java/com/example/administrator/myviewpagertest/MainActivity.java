1ae99d94d3d84c282f4baa8b19c8a2c4e0a7ad44
Brad Chao <brad@brad.tw>
2017-10-28 14:42:25 +0800

    201710281442

--- app/src/main/java/com/example/administrator/myviewpagertest/MainActivity.java
+++ app/src/main/java/com/example/administrator/myviewpagertest/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.administrator.myviewpagertest;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
+    private ArrayList<View> views;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,30 @@ public class MainActivity extends AppCompatActivity {
     private void initViewPager(){
         LayoutInflater inflater = LayoutInflater.from(this);
 
+        views = new ArrayList<>();
         View page1 = inflater.inflate(R.layout.page1, null);
         View page2 = inflater.inflate(R.layout.page2, null);
         View page3 = inflater.inflate(R.layout.page3, null);
+        views.add(page1);views.add(page2);views.add(page3);
+
+        viewPager.setAdapter(new MyPagerAdapter());
 
 
     }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+    }
+
 
 
 }
